5a799df096ee87fb0b84247f9831d8e0e241d6e5
lottestars <charlotteparfitt@outlook.com>
2021-06-21 21:55:51 +0100

    added the relevant icon from the api with live update

--- vanilla weather app/src/app.js
+++ vanilla weather app/src/app.js
@@ -28,15 +28,23 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "8f469bc7cfa94d62e817ad3980c0b7af";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=Leeds&appid=${apiKey}&units=metric`;
+let city = "leeds";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
