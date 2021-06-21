b4ae1e44ca2f9c25f01d21201be1e931c733fb26
lottestars <charlotteparfitt@outlook.com>
2021-06-21 17:38:03 +0100

    showing the current temperture

--- vanilla weather app/src/app.js
+++ vanilla weather app/src/app.js
@@ -1,1 +1,17 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather.description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "8f469bc7cfa94d62e817ad3980c0b7af";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=Leeds&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
